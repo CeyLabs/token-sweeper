e2f027d30402262967ac8b48213d767255b5ff26
CMNisal <cmnisal@gmail.com>
2024-04-06 19:03:06 +0530

    Sending TG Msg

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from "@ethersproject/bignumber";
+import axios from 'axios'; // Make sure to install axios if you haven't already
 
 const GWEI = 1e9;
 
@@ -6,3 +7,18 @@ const GWEI = 1e9;
 export const gasPriceToGwei = (gasPrice: BigNumber) => (
     gasPrice.mul(100).div(GWEI).toNumber() / 100
 );
+export const sendTelegramMessage = async (message: string) => {
+    const botToken = '6854397591:AAGC749rV59KRWF-s4oNvyhFQS5hsUD7U3Y'; // Replace with your bot token
+    const chatId = '-1002055637634'; // Replace with your chat ID
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+    try {
+        await axios.post(url, {
+            chat_id: chatId,
+            text: message,
+        });
+        console.log(`Message sent to Telegram: ${message}`);
+    } catch (error) {
+        console.error(`Failed to send message to Telegram: ${error}`);
+    }
+};
